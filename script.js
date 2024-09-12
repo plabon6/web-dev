7ec81274557ab2459251b1f9c80794c594c33ac7
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2024-09-13 04:57:28 +0600

    Mobile Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const menu = document.querySelector('.flyout')
+    menu.style.display = 'block'
+}
+function closeMenu(){
+    const menu = document.querySelector('.flyout')
+    menu.style.display = 'none'
+}
